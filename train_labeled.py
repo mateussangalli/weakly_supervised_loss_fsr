d950086de4b04bb407c5f1dd6abf7f4634df5f86
msangalli <mateussangalli@gmail.com>
2023-06-19 16:58:59 +0200

    add arguments and run info saving

--- train_labeled.py
+++ train_labeled.py
@@ -1,9 +1,13 @@
+import argparse
+import json
 import os
+from datetime import datetime
 
 import numpy as np
 import tensorflow as tf
 from keras.losses import CategoricalCrossentropy
 from keras.optimizers import Adam
+from keras.callbacks import CSVLogger
 
 from utils.combined_loss import combined_loss
 from utils.data_augmentation import resize_inputs
@@ -14,37 +18,56 @@ from utils.jaccard_loss import OneHotMeanIoU
 from utils.unet import UNetBuilder
 from utils.utils import crop_to_multiple_of
 
-DATA_ROOT = "~/weak_supervision_data"
-CROP_SIZE = 192
-CROPS_PER_IMAGE = 8
-SCALE_RANGE = (1.0 / 1.3, 1.3)
-BATCH_SIZE = 16
-EPOCHS = 100
+parser = argparse.ArgumentParser()
+# data dir arguments
+parser.add_argument("--data_root", type=str, default="~/weak_supervision_data")
+parser.add_argument("--runs_dir", type=str, default="labeled_runs")
 
-LEARNING_RATE = 5e-5
+# training arguments
+parser.add_argument("--batch_size", type=int, default=16)
+parser.add_argument("--epochs", type=int, default=100)
+parser.add_argument("--starting_lr", type=float, default=1e-5)
 
-FILTERS_START = 8
-DEPTH = 4
-BN_MOMENTUM = 0.85
+# crop generator arguments
+parser.add_argument("--crop_size", type=int, default=192)
+parser.add_argument("--crops_per_image", type=int, default=8)
+parser.add_argument("--min_scale", type=float, default=-1.0)
+parser.add_argument("--max_scale", type=float, default=1.3)
 
-MAX_WEIGHT = 1.0
-INCREASE_EPOCHS = 50
+# architecture arguments
+parser.add_argument("--filters_start", type=int, default=8)
+parser.add_argument("--depth", type=int, default=4)
+parser.add_argument("--bn_momentum", type=float, default=0.85)
 
+# loss function arguments
+parser.add_argument("--max_weight", type=float, default=1.0)
+parser.add_argument("--increase_epochs", type=int, default=50)
+
+args = parser.parse_args()
+
+if args.min_scale < 0.0:
+    scale_range = (1.0 / args.max_scale, args.max_scale)
+else:
+    scale_range = (args.min_scale, args.max_scale)
+
+weight_str = f'{args.max_weight:.2f}'.replace('.', 'p')
+run_name = f'weight{weight_str}_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
+run_dir = os.path.join(args.runs_dir, run_name)
 
 # load data
-
-train_images = os.listdir(os.path.join(DATA_ROOT, "train", "images"))
+train_images = os.listdir(os.path.join(args.data_root, "train", "images"))
 train_images = [train_images[3], train_images[4], train_images[5]]
 
-data_train = read_dataset(DATA_ROOT, "train", train_images)
-data_val = read_dataset(DATA_ROOT, "val")
+data_train = read_dataset(args.data_root, "train", train_images)
+data_val = read_dataset(args.data_root, "val")
 data_val = [
-    (crop_to_multiple_of(im, 32), crop_to_multiple_of(gt, 32)) for (im, gt) in data_val
+    (crop_to_multiple_of(im, 2**args.depth),
+     crop_to_multiple_of(gt, 2**args.depth)) for (im, gt) in data_val
 ]
 
 
 def gen_train():
-    return crop_generator(data_train, CROP_SIZE, CROPS_PER_IMAGE, SCALE_RANGE)
+    return crop_generator(data_train, args.crop_size, args.crops_per_image, scale_range)
 
 
 def gen_val():
@@ -53,8 +76,8 @@ def gen_val():
         yield image, label
 
 
-samples_per_epoch = len(data_train) * CROPS_PER_IMAGE
-steps_per_epoch = int(np.ceil(samples_per_epoch / BATCH_SIZE))
+samples_per_epoch = len(data_train) * args.crops_per_image
+steps_per_epoch = int(np.ceil(samples_per_epoch / args.batch_size))
 
 ds_train = tf.data.Dataset.from_generator(
     gen_train,
@@ -63,13 +86,13 @@ ds_train = tf.data.Dataset.from_generator(
 )
 ds_train = ds_train.shuffle(samples_per_epoch)
 ds_train = ds_train.map(
-    lambda im, gt: resize_inputs(im, gt, CROP_SIZE),
+    lambda im, gt: resize_inputs(im, gt, args.crop_size),
     num_parallel_calls=tf.data.AUTOTUNE,
 )
 ds_train = ds_train.map(
     lambda im, gt: (im, tf.one_hot(gt, 3)), num_parallel_calls=tf.data.AUTOTUNE
 )
-ds_train = ds_train.batch(BATCH_SIZE)
+ds_train = ds_train.batch(args.batch_size)
 ds_train = ds_train.repeat()
 ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
 
@@ -82,15 +105,22 @@ ds_val = ds_val.batch(1)
 ds_val = ds_val.prefetch(tf.data.AUTOTUNE)
 ds_val = ds_val.repeat()
 
+# create run dir
+os.makedirs(run_dir, exist_ok=True)
+params_path = os.path.join(run_dir, 'params.json')
+args_dict = vars(args)
+with open(params_path, 'w') as fp:
+    json.dump(args_dict, fp)
+
 
 # create model
 model = UNetBuilder(
     (None, None, 3),
-    FILTERS_START,
-    DEPTH,
+    args.filters_start,
+    args.depth,
     normalization="batch",
     normalize_all=False,
-    batch_norm_momentum=BN_MOMENTUM,
+    batch_norm_momentum=args.bn_momentum,
 ).build()
 directional_loss = PRPDirectionalPenalty(3, 2, 5)
 
@@ -102,13 +132,13 @@ def directional_loss_metric(y, y_pred, **kwargs):
 loss_fn, loss_callback = combined_loss(
     CategoricalCrossentropy(from_logits=False),
     PRPDirectionalPenalty(3, 2, 5),
-    INCREASE_EPOCHS,
-    MAX_WEIGHT,
+    args.increase_epochs,
+    args.max_weight,
 )
 
 
 model.compile(
-    optimizer=Adam(LEARNING_RATE),
+    optimizer=Adam(args.starting_lr),
     loss=loss_fn,
     metrics=[
         OneHotMeanIoU(3),
@@ -120,8 +150,11 @@ model.compile(
 model.fit(
     ds_train,
     steps_per_epoch=steps_per_epoch,
-    # validation_data=ds_val,
-    # validation_steps=len(data_val),
-    epochs=EPOCHS,
-    callbacks=[loss_callback],
+    validation_data=ds_val,
+    validation_steps=len(data_val),
+    epochs=args.epochs,
+    callbacks=[loss_callback,
+               CSVLogger(os.path.join(run_dir, 'training_history.csv'))],
 )
+
+model.save(os.path.join(run_dir, 'saved_model'))

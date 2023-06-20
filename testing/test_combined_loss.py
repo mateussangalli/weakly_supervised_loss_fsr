5f8679150fa47fa9f8665ffa951055c986e9f323
msangalli <mateussangalli@gmail.com>
2023-06-20 14:34:27 +0200

    change test to class definition of the loss

--- testing/test_combined_loss.py
+++ testing/test_combined_loss.py
@@ -2,7 +2,7 @@ import numpy as np
 import keras
 import tensorflow as tf
 
-from utils.combined_loss import combined_loss
+from utils.combined_loss import CombinedLoss
 
 
 if __name__ == "__main__":
@@ -11,7 +11,6 @@ if __name__ == "__main__":
     # Number of epochs you plan to train
     num_epochs = 10
 
-
     model = keras.models.Sequential()
     model.add(keras.layers.Conv2D(3, 3))
 
@@ -19,11 +18,11 @@ if __name__ == "__main__":
         return (tf.zeros_like(y_pred)+1e-5) * y_pred
 
     def loss2(y_pred):
-        return tf.ones_like(y_pred) * y_pred
+        return tf.ones_like(y_pred) + 1e-5 * y_pred
 
-    loss_fn, callback = combined_loss(loss1, loss2, num_epochs)
+    loss_fn = CombinedLoss(loss1, loss2, num_epochs)
 
     # Example usage during model.fit
     model.compile(optimizer="adam", loss=loss_fn)
 
-    model.fit(x_train, y_train, epochs=num_epochs, callbacks=[callback])
+    model.fit(x_train, y_train, epochs=num_epochs, callbacks=[loss_fn.callback])

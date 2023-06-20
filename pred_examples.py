99c174cfb878146849d02a6190418371b64ca2a1
msangalli <mateussangalli@gmail.com>
2023-06-20 13:27:58 +0200

    add CombinedLoss class

---
+++ pred_examples.py
@@ -0,0 +1,71 @@
+import argparse
+import os
+
+import numpy as np
+from keras.models import load_model
+from keras.losses import CategoricalCrossentropy
+import keras
+from skimage.io import imsave
+
+from utils.data_loading import read_dataset
+from utils.utils import crop_to_multiple_of
+from utils.jaccard_loss import OneHotMeanIoU
+from utils.combined_loss import CombinedLoss
+from utils.directional_relations import PRPDirectionalPenalty
+
+parser = argparse.ArgumentParser()
+# data dir arguments
+parser.add_argument("run_id", type=str)
+parser.add_argument("--data_root", type=str, default="../prp_loreal_data")
+parser.add_argument("--runs_dir", type=str, default="labeled_runs")
+
+args = parser.parse_args()
+
+
+run_dir = os.path.join(args.runs_dir, args.run_id)
+
+# load data
+val_images = os.listdir(os.path.join(args.data_root, "val", "images"))
+val_images = [val_images[3], val_images[4], val_images[5]]
+
+data_val = read_dataset(args.data_root, "val", val_images)
+data_val = [
+    (crop_to_multiple_of(im, 32),
+     crop_to_multiple_of(gt, 32)) for (im, gt) in data_val
+]
+
+
+def gen_val():
+    for image, label in data_val:
+        image = image.astype(np.float32) / 255.0
+        yield image, label
+
+
+directional_loss = PRPDirectionalPenalty(3, 2, 5)
+
+
+def directional_loss_metric(y, y_pred, **kwargs):
+    return directional_loss(y_pred)
+
+
+loss_fn = CombinedLoss(
+    CategoricalCrossentropy(from_logits=False),
+    PRPDirectionalPenalty(3, 2, 5),
+    50,
+    0.,
+)
+
+# create run dir
+custom_objects = {'OneHotMeanIoU': OneHotMeanIoU(3),
+                  'directional_loss_metric': directional_loss_metric,
+                  'PRPDirectionalPenalty': PRPDirectionalPenalty(3, 2, 5),
+                  'CombinedLoss': loss_fn}
+model = load_model(os.path.join(run_dir, 'saved_model'),
+                   custom_objects=custom_objects,
+                   compile=False)
+
+pred_dir = os.path.join(run_dir, 'predictions')
+os.mkdirs(pred_dir)
+for i, (im, _) in enumerate(data_val):
+    im = im.astype(np.float32) / 255.0
+    imsave(os.path.join(pred_dir, f'pred{i}.png'), im)
--- utils/combined_loss.py
+++ utils/combined_loss.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from keras.callbacks import LambdaCallback
+from keras.losses import Loss
 
 
 def combined_loss(loss_fn, reg_fn, num_epochs, max_weight=5.):
@@ -17,3 +18,24 @@ def combined_loss(loss_fn, reg_fn, num_epochs, max_weight=5.):
         return loss_fn(y_true, y_pred) + weight * reg_fn(y_pred)
 
     return loss_function, epoch_callback
+
+
+class CombinedLoss(Loss):
+    def __init__(self, loss_fn, reg_fn, num_epochs, max_weight=5., **kwargs):
+        super().__init__(**kwargs)
+        self.weight = tf.Variable((0.,), shape=(1,), trainable=False)
+        self.loss_fn = loss_fn
+        self.reg_fn = reg_fn
+        self.num_epochs = num_epochs
+        self.max_weight = max_weight
+
+        # Create a callback to update the epoch value
+        def weight_function(epoch, logs):
+            w = min((epoch + 1) / num_epochs, 1.)
+            w = max_weight * w
+            self.weight.assign((w,))
+
+        self.callback = LambdaCallback(on_epoch_begin=weight_function)
+
+    def __call__(self, y_true, y_pred, sample_weight=None):
+        return self.loss_fn(y_true, y_pred) + self.weight * self.reg_fn(y_pred)

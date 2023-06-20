d1e88a6f199cdda6615d2602f3fa663a35a2d8a8
msangalli <mateussangalli@gmail.com>
2023-06-20 14:25:28 +0200

    add unittests for model loading with different configurations

--- testing/test_model_loading.py
+++ testing/test_model_loading.py
@@ -1,75 +1,161 @@
 import argparse
 import os
+import shutil
+import unittest
 
 import numpy as np
+from keras.losses import CategoricalCrossentropy, categorical_crossentropy
 from keras.models import load_model
-from keras.losses import CategoricalCrossentropy
 from keras.optimizers import Adam
 
-from utils.unet import UNetBuilder
-from utils.data_loading import read_dataset
-from utils.utils import crop_to_multiple_of
 from utils.combined_loss import CombinedLoss
 from utils.directional_relations import PRPDirectionalPenalty
-
-parser = argparse.ArgumentParser()
-# data dir arguments
-parser.add_argument("run_id", type=str)
-parser.add_argument("--data_root", type=str, default="~/weak_supervision_data")
-parser.add_argument("--runs_dir", type=str, default="labeled_runs")
-
-args = parser.parse_args()
+from utils.jaccard_loss import OneHotMeanIoU
+from utils.unet import UNetBuilder
 
 
-run_dir = os.path.join(args.runs_dir, args.run_id)
+class TestLoading(unittest.TestCase):
+    def test_without_metrics(self):
+        loss_fn = CombinedLoss(
+            CategoricalCrossentropy(from_logits=False),
+            PRPDirectionalPenalty(3, 2, 5),
+            50,
+            0.0,
+        )
 
-# load data
-val_images = os.listdir(os.path.join(args.data_root, "val", "images"))
-val_images = [val_images[3], val_images[4], val_images[5]]
+        model = UNetBuilder(
+            (None, None, 3),
+            8,
+            4,
+            normalization="batch",
+            normalize_all=False,
+            batch_norm_momentum=0.85,
+        ).build()
+        model.compile(
+            optimizer=Adam(),
+            loss=loss_fn,
+        )
+        model.save("test_model")
 
-data_val = read_dataset(args.data_root, "val", val_images)
-data_val = [
-    (crop_to_multiple_of(im, 32),
-     crop_to_multiple_of(gt, 32)) for (im, gt) in data_val
-]
+        # create run dir
+        custom_objects = {"CombinedLoss": loss_fn}
+        model = load_model("test_model", custom_objects=custom_objects, compile=False)
+
+        shutil.rmtree("test_model", ignore_errors=False, onerror=None)
+
+    def test_with_directional_metric(self):
+        directional_loss = PRPDirectionalPenalty(3, 2, 5)
+
+        def directional_loss_metric(y, y_pred, **kwargs):
+            return directional_loss(y_pred)
+
+        loss_fn = CombinedLoss(
+            CategoricalCrossentropy(from_logits=False),
+            PRPDirectionalPenalty(3, 2, 5),
+            50,
+            0.0,
+        )
+
+        model = UNetBuilder(
+            (None, None, 3),
+            8,
+            4,
+            normalization="batch",
+            normalize_all=False,
+            batch_norm_momentum=0.85,
+        ).build()
+        model.compile(optimizer=Adam(), loss=loss_fn, metrics=[directional_loss_metric])
+        model.save("test_model")
+
+        # create run dir
+        custom_objects = {
+            "CombinedLoss": loss_fn,
+            "directional_loss_metric": directional_loss_metric,
+        }
+        model = load_model("test_model", custom_objects=custom_objects, compile=False)
+
+        shutil.rmtree("test_model", ignore_errors=False, onerror=None)
+
+    def test_with_iou_metric(self):
+        directional_loss = PRPDirectionalPenalty(3, 2, 5)
+
+        def directional_loss_metric(y, y_pred, **kwargs):
+            return directional_loss(y_pred)
+
+        loss_fn = CombinedLoss(
+            CategoricalCrossentropy(from_logits=False),
+            PRPDirectionalPenalty(3, 2, 5),
+            50,
+            0.0,
+        )
+
+        model = UNetBuilder(
+            (None, None, 3),
+            8,
+            4,
+            normalization="batch",
+            normalize_all=False,
+            batch_norm_momentum=0.85,
+        ).build()
+        model.compile(
+            optimizer=Adam(),
+            loss=loss_fn,
+            metrics=[directional_loss_metric, OneHotMeanIoU(3)],
+        )
+        model.save("test_model")
+
+        # create run dir
+        custom_objects = {
+            "CombinedLoss": loss_fn,
+            "OneHotMeanIoU": OneHotMeanIoU(3),
+        }
+        model = load_model("test_model", custom_objects=custom_objects, compile=False)
+
+        shutil.rmtree("test_model", ignore_errors=False, onerror=None)
+
+    def test_with_crossentropy(self):
+        directional_loss = PRPDirectionalPenalty(3, 2, 5)
+
+        def directional_loss_metric(y, y_pred, **kwargs):
+            return directional_loss(y_pred)
+
+        loss_fn = CombinedLoss(
+            CategoricalCrossentropy(from_logits=False),
+            PRPDirectionalPenalty(3, 2, 5),
+            50,
+            0.0,
+        )
+
+        model = UNetBuilder(
+            (None, None, 3),
+            8,
+            4,
+            normalization="batch",
+            normalize_all=False,
+            batch_norm_momentum=0.85,
+        ).build()
+
+        crossentropy = CategoricalCrossentropy(from_logits=False)
+
+        def crossentropy_metric(y_true, y_pred, **kwargs):
+            crossentropy(y_true, y_pred)
+
+        model.compile(
+            optimizer=Adam(),
+            loss=loss_fn,
+            metrics=[directional_loss_metric, crossentropy_metric],
+        )
+        model.save("test_model")
+
+        # create run dir
+        custom_objects = {
+            "CombinedLoss": loss_fn,
+            "crossentropy_metric": crossentropy_metric,
+        }
+        model = load_model("test_model", custom_objects=custom_objects, compile=False)
+
+        shutil.rmtree("test_model", ignore_errors=False, onerror=None)
 
 
-def gen_val():
-    for image, label in data_val:
-        image = image.astype(np.float32) / 255.0
-        yield image, label
-
-
-directional_loss = PRPDirectionalPenalty(3, 2, 5)
-
-
-def directional_loss_metric(y, y_pred, **kwargs):
-    return directional_loss(y_pred)
-
-
-loss_fn = CombinedLoss(
-    CategoricalCrossentropy(from_logits=False),
-    PRPDirectionalPenalty(3, 2, 5),
-    50,
-    0.,
-)
-
-model = UNetBuilder(
-    (None, None, 3),
-    8,
-    4,
-    normalization="batch",
-    normalize_all=False,
-    batch_norm_momentum=0.85,
-).build()
-model.compile(
-    optimizer=Adam(args.starting_lr),
-    loss=loss_fn,
-)
-model.save('test_model')
-
-# create run dir
-custom_objects = {'CombinedLoss': loss_fn}
-model = load_model(os.path.join(run_dir, 'saved_model'),
-                   custom_objects=custom_objects,
-                   compile=False)
+if __name__ == "__main__":
+    unittest.main()

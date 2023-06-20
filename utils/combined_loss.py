c3b46665d6fe737c967b3dbed0fd2c3d63c43432
Santiago Velasco-Forero <velasco@cmm.ensmp.fr>
2023-06-20 12:01:09 +0200

    Update combined_loss.py
    Add a simple extension for multiple regularizations losses

--- utils/combined_loss.py
+++ utils/combined_loss.py
@@ -17,3 +17,28 @@ def combined_loss(loss_fn, reg_fn, num_epochs, max_weight=5.):
         return loss_fn(y_true, y_pred) + weight * reg_fn(y_pred)
 
     return loss_function, epoch_callback
+
+
+def combined_loss_multiple(loss_fn, reg_fns, num_epochs=[50,50], max_weight=[5.,5.]):
+  W=[]
+  for i in range(len(reg_fns)):
+    W.append(tf.Variable((0.,), shape=(1,), trainable=False))
+  # Create a callback to update the epoch value
+  def weight_function(epoch, logs):
+    for i in range(len(max_weight)):
+      w = min((epoch + 1) / num_epochs[i], 1.)
+      w = max_weight[i] * w
+      W[i].assign((w,))
+
+  epoch_callback=[]
+  for i in range(len(reg_fns)):
+    epoch_callback.append(LambdaCallback(on_epoch_begin=weight_function))
+
+
+  def loss_function(y_true, y_pred):
+    loss=loss_fn(y_true, y_pred)
+    for i,reg_fn in enumerate(reg_fns):
+      loss=loss + W[i]*reg_fn(y_pred)
+    return loss
+    
+  return loss_function, epoch_callback

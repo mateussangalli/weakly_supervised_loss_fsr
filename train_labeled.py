d333b141150e7363b84f672a0881176043058d41
msangalli <mateussangalli@gmail.com>
2023-06-22 23:54:05 +0200

    change default arguments

--- train_labeled.py
+++ train_labeled.py
@@ -20,23 +20,23 @@ from utils.utils import crop_to_multiple_of
 
 parser = argparse.ArgumentParser()
 # data dir arguments
-parser.add_argument("--run_id", type=str, default='')
+parser.add_argument("--run_id", type=str, default="")
 parser.add_argument("--data_root", type=str, default="~/weak_supervision_data")
 parser.add_argument("--runs_dir", type=str, default="labeled_runs")
 
 # training arguments
-parser.add_argument("--num_images", type=int, default=5)
+parser.add_argument("--num_images_labeled", type=int, default=3)
 parser.add_argument("--batch_size", type=int, default=64)
 parser.add_argument("--epochs", type=int, default=300)
 parser.add_argument("--starting_lr", type=float, default=1e-4)
 parser.add_argument("--val_freq", type=int, default=1)
 parser.add_argument("--weight_decay", type=float, default=1e-4)
-parser.add_argument("--lr_decay_rate", type=float, default=0.03)
-parser.add_argument("--rotation_angle", type=float, default=np.pi/8.)
+parser.add_argument("--lr_decay_rate", type=float, default=0.003)
+parser.add_argument("--rotation_angle", type=float, default=np.pi / 8.0)
 
 # crop generator arguments
 parser.add_argument("--crop_size", type=int, default=192)
-parser.add_argument("--crops_per_image", type=int, default=8)
+parser.add_argument("--crops_per_image", type=int, default=16)
 parser.add_argument("--min_scale", type=float, default=-1.0)
 parser.add_argument("--max_scale", type=float, default=1.3)
 
@@ -62,8 +62,8 @@ if args.min_scale < 0.0:
 else:
     scale_range = (args.min_scale, args.max_scale)
 
-if args.run_id == '':
-    weight_str = f'{args.max_weight:.4f}'.replace('.', 'p')
+if args.run_id == "":
+    weight_str = f"{args.max_weight:.4f}".replace(".", "p")
     run_name = f'weight{weight_str}_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
 else:
     run_name = args.run_id
@@ -71,15 +71,21 @@ run_dir = os.path.join(args.runs_dir, run_name)
 
 # load data
 train_images = os.listdir(os.path.join(args.data_root, "train", "images"))
-if args.num_images > 0:
-    train_images = [train_images[3], train_images[4],
-                    train_images[5], train_images[34], train_images[64]][:args.num_images]
+if args.num_images_labeled > 0:
+    train_images = [
+        train_images[3],
+        train_images[34],
+        train_images[64],
+        train_images[4],
+        train_images[5],
+    ][: args.num_images_labeled]
 
 data_train = read_dataset(args.data_root, "train", train_images)
 data_val = read_dataset(args.data_root, "val")
 data_val = [
     (crop_to_multiple_of(im, 2**args.depth),
-     crop_to_multiple_of(gt, 2**args.depth)) for (im, gt) in data_val
+     crop_to_multiple_of(gt, 2**args.depth))
+    for (im, gt) in data_val
 ]
 
 
@@ -107,9 +113,9 @@ ds_val = ds_val.repeat()
 
 # create run dir
 os.makedirs(run_dir, exist_ok=True)
-params_path = os.path.join(run_dir, 'params.json')
+params_path = os.path.join(run_dir, "params.json")
 args_dict = vars(args)
-with open(params_path, 'w') as fp:
+with open(params_path, "w") as fp:
     json.dump(args_dict, fp)
 
 
@@ -122,9 +128,9 @@ model = UNetBuilder(
     normalize_all=False,
     batch_norm_momentum=args.bn_momentum,
 ).build()
-directional_loss = PRPDirectionalPenalty(args.strel_size,
-                                         args.strel_spread,
-                                         args.strel_iterations)
+directional_loss = PRPDirectionalPenalty(
+    args.strel_size, args.strel_spread, args.strel_iterations
+)
 
 
 def directional_loss_metric(y, y_pred, **kwargs):
@@ -140,9 +146,8 @@ def crossentropy_metric(y_true, y_pred, **kwargs):
 
 loss_fn = CombinedLoss(
     CategoricalCrossentropy(from_logits=False),
-    PRPDirectionalPenalty(args.strel_size,
-                          args.strel_spread,
-                          args.strel_iterations),
+    PRPDirectionalPenalty(
+        args.strel_size, args.strel_spread, args.strel_iterations),
     args.increase_epochs,
     args.max_weight,
 )
@@ -150,7 +155,8 @@ loss_fn = CombinedLoss(
 
 model.compile(
     optimizer=tf.keras.optimizers.experimental.Adam(
-        args.starting_lr, weight_decay=args.weight_decay),
+        args.starting_lr, weight_decay=args.weight_decay
+    ),
     loss=loss_fn,
     metrics=[
         OneHotMeanIoU(3),
@@ -165,17 +171,20 @@ def schedule(epoch, lr):
         return lr * tf.exp(-args.lr_decay_rate)
     return lr
 
+
 model.fit(
     ds_train,
     steps_per_epoch=steps_per_epoch,
     validation_data=ds_val,
     validation_steps=len(data_val),
     epochs=args.epochs,
-    callbacks=[loss_fn.callback,
-               CSVLogger(os.path.join(run_dir, 'training_history.csv')),
-               LearningRateScheduler(schedule)],
+    callbacks=[
+        loss_fn.callback,
+        CSVLogger(os.path.join(run_dir, "training_history.csv")),
+        LearningRateScheduler(schedule),
+    ],
     verbose=args.verbose,
-    validation_freq=args.val_freq
+    validation_freq=args.val_freq,
 )
 
-model.save(os.path.join(run_dir, 'saved_model'))
+model.save(os.path.join(run_dir, "saved_model"))

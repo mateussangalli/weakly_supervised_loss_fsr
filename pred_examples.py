09dd4c82172c446cb3d467af62e36cd24c2065e1
msangalli <mateussangalli@gmail.com>
2023-06-22 10:04:33 +0200

    compute post-processing as well

--- pred_examples.py
+++ pred_examples.py
@@ -1,7 +1,6 @@
 import argparse
 import os
 
-import keras
 import numpy as np
 from keras.losses import CategoricalCrossentropy
 from keras.models import load_model
@@ -10,6 +9,7 @@ from skimage.io import imsave
 from utils.combined_loss import CombinedLoss
 from utils.data_loading import read_dataset, save_label
 from utils.directional_relations import PRPDirectionalPenalty
+from utils.im_tools import TwoLayers
 from utils.jaccard_loss import OneHotMeanIoU
 from utils.utils import pad_to_multiple_of
 
@@ -27,7 +27,7 @@ run_dir = os.path.join(args.runs_dir, args.run_id)
 # load data
 filenames = os.listdir(os.path.join(args.data_root, args.subset, "images"))
 
-data = read_dataset(args.data_root, args.subset, filenames)
+data = read_dataset(args.data_root, args.subset, filenames, min_size=(0, 0))
 
 
 def gen_val():
@@ -66,10 +66,22 @@ model = load_model(os.path.join(run_dir, 'saved_model'),
                    custom_objects=custom_objects,
                    compile=False)
 
+postproc_func = TwoLayers(3, ordered=True)
+
+
+def postproc(pred):
+    pred = np.array([2, 1, 3])[pred]
+    pred = postproc_func(pred)
+    pred = np.array([1, 0, 2])[pred-1]
+    return pred
+
+
 proba_dir = os.path.join(run_dir, 'probability_maps', args.subset)
 pred_dir = os.path.join(run_dir, 'predictions', args.subset)
+post_dir = os.path.join(run_dir, 'postproc', args.subset)
 os.makedirs(pred_dir, exist_ok=True)
 os.makedirs(proba_dir, exist_ok=True)
+os.makedirs(post_dir, exist_ok=True)
 for i, ((im, _), name) in enumerate(zip(data, filenames)):
     im, (pad1, pad2) = pad_to_multiple_of(im, 32)
     im = im.astype(np.float32) / 255.0
@@ -77,5 +89,7 @@ for i, ((im, _), name) in enumerate(zip(data, filenames)):
     proba = np.array(proba)[0, :-pad1, :-pad2, ...]
     proba = (proba * 255.).astype(np.uint8)
     pred = np.argmax(proba, -1)
+    post = postproc(pred)
     imsave(os.path.join(proba_dir, name), proba)
     save_label(pred, os.path.join(pred_dir, name))
+    save_label(post, os.path.join(post_dir, name))

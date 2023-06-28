c58070adc038ded5f94699cf0493008a8ec7baf7
msangalli <mateussangalli@gmail.com>
2023-06-28 19:27:25 +0200

    fix bugs with SemiSupModel

---
+++ testing/test_unlabeled_training.py
@@ -0,0 +1,130 @@
+import unittest
+import shutil
+import numpy as np
+
+import tensorflow as tf
+from keras.layers import Dense, Input
+from keras.models import load_model
+
+from utils.unlabeled_training import SemiSupModel
+
+
+class TestUnlabeledTraining(unittest.TestCase):
+    def test_model_creation(self):
+        input_layer = Input((5,))
+        out_layer = Dense(10)(input_layer)
+        model = SemiSupModel(input_layer, out_layer, alpha=1.)
+        inputs = tf.random.uniform([20, 5])
+        out = model(inputs)
+        self.assertEqual(out.shape[0], 20)
+        self.assertEqual(out.shape[1], 10)
+
+    def test_model_compiling(self):
+        input_layer = Input((5,))
+        out_layer = Dense(10)(input_layer)
+        model = SemiSupModel(input_layer, out_layer, alpha=1.)
+        def loss2(pred): return tf.reduce_sum(tf.abs(pred))
+        model.compile('adam', 'mse', loss2)
+        inputs = tf.random.uniform([20, 5])
+        out = model(inputs)
+        self.assertEqual(out.shape[0], 20)
+        self.assertEqual(out.shape[1], 10)
+
+    def test_model_fitting(self):
+        input_layer = Input((5,))
+        out_layer = Dense(10)(input_layer)
+        model = SemiSupModel(input_layer, out_layer, alpha=1.)
+        def loss2(pred): return tf.reduce_sum(tf.abs(pred))
+        model.compile('adam', 'mse', loss2)
+
+        inputs_l = tf.random.uniform([20, 5])
+        outputs = tf.random.uniform([20, 10])
+        inputs_u = tf.random.uniform([40, 5])
+
+        ds_l = tf.data.Dataset.from_tensor_slices((inputs_l, outputs)).batch(4)
+        ds_u = tf.data.Dataset.from_tensor_slices(inputs_u).batch(8)
+        ds = tf.data.Dataset.zip((ds_l, ds_u))
+
+        model.fit(ds, epochs=2, verbose=0)
+
+    def test_model_io(self):
+        input_layer = Input((5,))
+        out_layer = Dense(10)(input_layer)
+        model = SemiSupModel(input_layer, out_layer, alpha=1.)
+        def loss2(pred): return tf.reduce_sum(tf.abs(pred))
+        model.compile('adam', 'mse', loss2)
+
+        inputs_l = tf.random.uniform([20, 5])
+        outputs = tf.random.uniform([20, 10])
+        inputs_u = tf.random.uniform([40, 5])
+
+        ds_l = tf.data.Dataset.from_tensor_slices((inputs_l, outputs)).batch(4)
+        ds_u = tf.data.Dataset.from_tensor_slices(inputs_u).batch(8)
+        ds = tf.data.Dataset.zip((ds_l, ds_u))
+
+        model.fit(ds, epochs=2, verbose=0)
+        weights_b4 = model.get_weights()
+
+        model.save("test_model")
+        custom_objects = {"SemiSupModel": SemiSupModel}
+        model = load_model(
+            "test_model", custom_objects=custom_objects, compile=False)
+        shutil.rmtree("test_model", ignore_errors=False, onerror=None)
+
+        weights_after = model.get_weights()
+
+        for w1, w2 in zip(weights_b4, weights_after):
+            dist = np.sum(np.abs(w1 - w2))
+            self.assertAlmostEqual(dist, 0.)
+
+    def test_model_eval(self):
+        input_layer = Input((5,))
+        out_layer = Dense(10)(input_layer)
+        model = SemiSupModel(input_layer, out_layer, alpha=1.)
+        def loss2(pred): return tf.reduce_sum(tf.abs(pred))
+        model.compile('adam', 'mse', loss2, metrics=['mse'])
+
+        inputs_l = tf.random.uniform([20, 5])
+        outputs = tf.random.uniform([20, 10])
+        inputs_u = tf.random.uniform([40, 5])
+        inputs_l_val = tf.random.uniform([20, 5])
+        outputs_val = tf.random.uniform([20, 10])
+
+        ds_l = tf.data.Dataset.from_tensor_slices((inputs_l, outputs)).batch(4)
+        ds_u = tf.data.Dataset.from_tensor_slices(inputs_u).batch(8)
+        ds = tf.data.Dataset.zip((ds_l, ds_u))
+        ds_val = tf.data.Dataset.from_tensor_slices(
+            (inputs_l_val, outputs_val)).batch(4)
+
+        model.fit(ds, epochs=2, validation_data=ds_val)
+        model.evaluate(ds_val)
+
+    def test_schedule(self):
+        def alpha_schedule(t):
+            return tf.cast(t // 5, tf.float32)
+
+        def loss1(y, y_pred): return tf.reduce_sum(tf.abs(y_pred + y)) * 1e-10
+        def loss2(y_pred): return tf.reduce_sum(tf.abs(y_pred)) * 1e-10 + 1.
+
+        input_layer = Input((5,))
+        out_layer = Dense(10)(input_layer)
+        model = SemiSupModel(input_layer, out_layer, alpha=alpha_schedule)
+        model.compile('adam', loss1, loss2)
+
+        inputs_l = tf.random.uniform([20, 5])
+        outputs = tf.random.uniform([20, 10])
+        inputs_u = tf.random.uniform([40, 5])
+
+        ds_l = tf.data.Dataset.from_tensor_slices((inputs_l, outputs)).batch(4)
+        ds_u = tf.data.Dataset.from_tensor_slices(inputs_u).batch(8)
+        ds = tf.data.Dataset.zip((ds_l, ds_u))
+
+        history = model.fit(ds, epochs=15, verbose=0)
+        expected = np.arange(15).astype(np.float32)
+        results = np.array(history.history['total_loss'])
+        dist = np.sum(np.abs(expected - results))
+        self.assertAlmostEqual(dist, 0.)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- utils/unlabeled_training.py
+++ utils/unlabeled_training.py
@@ -2,18 +2,24 @@ import tensorflow as tf
 
 
 class SemiSupModel(tf.keras.Model):
-    def __init__(self, *args, alpha_schedule, **kwargs):
+    def __init__(self, *args, alpha=0., **kwargs):
         super(SemiSupModel, self).__init__(*args, **kwargs)
-        self.alpha_schedule = alpha_schedule
+        if isinstance(alpha, float):
+            self.alpha_schedule = lambda _: alpha
+        else:
+            self.alpha_schedule = alpha
+
 
     def compile(self, optimizer, loss_1, loss_2, **kwargs):
+        # WARN: if you load this model you probably have to call this function again
+        # WARN: also when you cal evaluate it will not use any of these losses
         super(SemiSupModel, self).compile(**kwargs)
-        self.optimizer = optimizer
-        self.loss_1 = loss_1
-        self.loss_2 = loss_2
+        self.optimizer = tf.keras.optimizers.get(optimizer)
+        self.loss_1 = tf.keras.losses.get(loss_1)
+        self.loss_2 = tf.keras.losses.get(loss_2)
 
     def train_step(self, data):
-        x_l, y, x_u = data
+        (x_l, y), x_u = data
 
         with tf.GradientTape() as tape:
             # Forward pass on labeled data
@@ -22,11 +28,10 @@ class SemiSupModel(tf.keras.Model):
 
             # Forward pass on unlabeled data
             y_u_pred = self(x_u, training=True)
-            loss_2_value = self.loss_2(x_u, y_u_pred)
+            loss_2_value = self.loss_2(y_u_pred)
 
             # Compute total loss with weight alpha(epoch)
-            alpha = self.alpha_schedule(
-                tf.keras.backend.get_value(self.optimizer.iterations))
+            alpha = self.alpha_schedule(self.optimizer.iterations)
             total_loss = loss_1_value + alpha * loss_2_value
 
         # Compute gradients and update weights
@@ -42,4 +47,5 @@ class SemiSupModel(tf.keras.Model):
         results['loss_1'] = loss_1_value
         results['loss_2'] = loss_2_value
         results['total_loss'] = total_loss
+
         return results

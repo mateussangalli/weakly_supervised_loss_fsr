42eea5dfaf2f34f6bcc1e3bc8f4e9c0b86d36ecd
msangalli <mateussangalli@gmail.com>
2023-06-28 20:17:41 +0200

    add unlabeled training module

--- train_unlabeled_reg.py
+++ train_unlabeled_reg.py
@@ -1,33 +1,31 @@
 import argparse
-from datetime import datetime
 import json
 import os
+from datetime import datetime
 
-from keras.callbacks import CSVLogger, LearningRateScheduler
-from keras.losses import CategoricalCrossentropy
 import numpy as np
 import tensorflow as tf
+from keras.callbacks import CSVLogger, LearningRateScheduler
+from keras.losses import CategoricalCrossentropy
 
-from utils.combined_loss import CombinedLoss
 from utils.data_generation import get_tf_train_dataset
-from utils.data_loading import read_dataset, read_dataset_pseudo
+from utils.data_loading import read_dataset
 from utils.directional_relations import PRPDirectionalPenalty
 from utils.jaccard_loss import OneHotMeanIoU
-from utils.unet import UNetBuilder
+from utils.unet import SemiSupUNetBuilder
 from utils.utils import crop_to_multiple_of
 
 parser = argparse.ArgumentParser()
 # data dir arguments
-parser.add_argument("pseudo_labels_dir", type=str)
 parser.add_argument("--run_id", type=str, default='')
 parser.add_argument("--data_root", type=str, default="../prp_loreal_data")
-parser.add_argument("--runs_dir", type=str, default="pseudo_labels_runs")
+parser.add_argument("--runs_dir", type=str, default="no_pseudo_runs")
 
 # training arguments
-# WARN: make sure that batch_size_labeled divides batch_size_pseudo
+# WARN: make sure that batch_size_labeled divides batch_size_unlabeled
 parser.add_argument("--num_images_labeled", type=int, default=3)
 parser.add_argument("--batch_size_labeled", type=int, default=16)
-parser.add_argument("--batch_size_pseudo", type=int, default=48)
+parser.add_argument("--batch_size_unlabeled", type=int, default=48)
 parser.add_argument("--epochs", type=int, default=30)
 parser.add_argument("--starting_lr", type=float, default=1e-4)
 parser.add_argument("--val_freq", type=int, default=1)
@@ -37,10 +35,10 @@ parser.add_argument("--rotation_angle", type=float, default=np.pi/8.)
 
 # crop generator arguments
 parser.add_argument("--crop_size", type=int, default=160)
-# WARN: please choose a number of crops_per_image_pseudo such that
-#  num_images_pseudo * crops_per_image_pseudo * batch_size_labeled is a multiple of
-#  num_images_labeled * batch_size_pseudo
-parser.add_argument("--crops_per_image_pseudo", type=int, default=3)
+# WARN: please choose a number of crops_per_image_unlabeled such that
+#  num_images_unlabeled * crops_per_image_unlabeled * batch_size_labeled is a multiple of
+#  num_images_labeled * batch_size_unlabeled
+parser.add_argument("--crops_per_image_unlabeled", type=int, default=3)
 parser.add_argument("--min_scale", type=float, default=-1.0)
 parser.add_argument("--max_scale", type=float, default=1.3)
 
@@ -88,36 +86,39 @@ else:
 
 
 data_train = read_dataset(args.data_root, "train", train_images_labeled)
-data_pseudo = read_dataset_pseudo(
-    args.data_root, "train", args.pseudo_labels_dir)
+data_unlabeled = read_dataset(args.data_root, "train")
+# just to be sure...
+data_unlabeled = [(x, np.zeros_like(y)) for x, y in data_unlabeled]
+
 data_val = read_dataset(args.data_root, "val")
 data_val = [
     (crop_to_multiple_of(im, 2**args.depth),
      crop_to_multiple_of(gt, 2**args.depth)) for (im, gt) in data_val
 ]
 
-# NOTE: we want the same number of labeled and pseudo-labeled data batches
+# NOTE: we want the same number of labeled and unlabeled data batches
 # NOTE: the number of batches is ceil(len(data) * crops_per_image / batch_size)
 # NOTE: Everything else is determined, so we must find crops_per_image_labeled
-crops_per_image_labeled = len(data_pseudo)
-crops_per_image_labeled *= args.crops_per_image_pseudo
+crops_per_image_labeled = len(data_unlabeled)
+crops_per_image_labeled *= args.crops_per_image_unlabeled
 crops_per_image_labeled *= args.batch_size_labeled
-crops_per_image_labeled //= len(data_train) * args.batch_size_pseudo
+crops_per_image_labeled //= len(data_train) * args.batch_size_unlabeled
 
 samples_per_epoch_label = crops_per_image_labeled * len(data_train)
-samples_per_epoch_pseudo = args.crops_per_image_pseudo * len(data_pseudo)
+samples_per_epoch_unlabeled = args.crops_per_image_unlabeled * \
+    len(data_unlabeled)
 steps_per_epoch_labeled = int(
     np.ceil(samples_per_epoch_label / args.batch_size_labeled))
-steps_per_epoch_pseudo = int(
-    np.ceil(samples_per_epoch_pseudo / args.batch_size_pseudo))
+steps_per_epoch_unlabeled = int(
+    np.ceil(samples_per_epoch_unlabeled / args.batch_size_unlabeled))
 # verify that everything is at it should be
-if steps_per_epoch_pseudo != steps_per_epoch_labeled:
+if steps_per_epoch_unlabeled != steps_per_epoch_labeled:
     msg1 = f'labeled: \n crops/image: {crops_per_image_labeled}, num images: {len(data_train)}, batch_size: {args.batch_size_labeled}'
-    msg2 = f'pseudo: \n crops/image: {args.crops_per_image_pseudo}, num images: {len(data_pseudo)}, batch_size: {args.batch_size_pseudo}'
+    msg2 = f'unlabeled: \n crops/image: {args.crops_per_image_unlabeled}, num images: {len(data_unlabeled)}, batch_size: {args.batch_size_unlabeled}'
     raise ValueError(
         'number of steps is wrong! \n' + msg1 + '\n' + msg2
     )
-steps_per_epoch = steps_per_epoch_pseudo
+steps_per_epoch = steps_per_epoch_unlabeled
 
 params_labeled = {
     "min_scale": args.min_scale,
@@ -127,21 +128,21 @@ params_labeled = {
     "crops_per_image": crops_per_image_labeled,
     "batch_size": args.batch_size_labeled
 }
-params_pseudo = {
+params_unlabeled = {
     "min_scale": args.min_scale,
     "max_scale": args.max_scale,
     "crop_size": args.crop_size,
     "rotation_angle": args.rotation_angle,
-    "crops_per_image": args.crops_per_image_pseudo,
-    "batch_size": args.batch_size_pseudo
+    "crops_per_image": args.crops_per_image_unlabeled,
+    "batch_size": args.batch_size_unlabeled
 }
 
-ds_train_labeled = get_tf_train_dataset(data_train, params_labeled)
-ds_train_pseudo = get_tf_train_dataset(data_pseudo, params_pseudo)
-ds_zip = tf.data.Dataset.zip((ds_train_labeled, ds_train_pseudo))
-ds_train = ds_zip.map(lambda a, b: (tf.concat((a[0], b[0]), 0), tf.concat((a[1], b[1]), 0)),
-                      num_parallel_calls=tf.data.AUTOTUNE)
-ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
+ds_train_labeled: tf.data.Dataset = get_tf_train_dataset(data_train, params_labeled)
+ds_train_unlabeled: tf.data.Dataset = get_tf_train_dataset(data_unlabeled, params_unlabeled)
+ds_train_unlabeled = ds_train_unlabeled.map(
+    lambda x, y: x, num_parallel_calls=tf.data.AUTOTUNE)
+ds_zip = tf.data.Dataset.zip((ds_train_labeled, ds_train_unlabeled))
+ds_train = ds_zip.prefetch(tf.data.AUTOTUNE)
 
 
 def gen_val():
@@ -167,14 +168,22 @@ with open(params_path, 'w') as fp:
     json.dump(args_dict, fp)
 
 
+increase_ratio = args.max_weight / float(args.increase_epochs)
+
+
+def alpha_schedule(t):
+    return tf.minimum(tf.cast(t, tf.float32) * increase_ratio, args.max_weight)
+
+
 # create model
-model = UNetBuilder(
+model = SemiSupUNetBuilder(
     (None, None, 3),
     args.filters_start,
     args.depth,
     normalization="batch",
     normalize_all=False,
     batch_norm_momentum=args.bn_momentum,
+    alpha=alpha_schedule
 ).build()
 directional_loss = PRPDirectionalPenalty(args.strel_size,
                                          args.strel_spread,
@@ -192,20 +201,17 @@ def crossentropy_metric(y_true, y_pred, **kwargs):
     return crossentropy(y_true, y_pred)
 
 
-loss_fn = CombinedLoss(
-    CategoricalCrossentropy(from_logits=False),
-    PRPDirectionalPenalty(args.strel_size,
-                          args.strel_spread,
-                          args.strel_iterations),
-    args.increase_epochs,
-    args.max_weight,
-)
+loss1 = CategoricalCrossentropy(from_logits=False)
+loss2 = PRPDirectionalPenalty(args.strel_size,
+                              args.strel_spread,
+                              args.strel_iterations),
 
 
 model.compile(
-    optimizer=tf.keras.optimizers.experimental.Adam(
+    tf.keras.optimizers.experimental.Adam(
         args.starting_lr, weight_decay=args.weight_decay),
-    loss=loss_fn,
+    loss1,
+    loss2,
     metrics=[
         OneHotMeanIoU(3),
         crossentropy_metric,
@@ -226,8 +232,7 @@ model.fit(
     validation_data=ds_val,
     validation_steps=len(data_val),
     epochs=args.epochs,
-    callbacks=[loss_fn.callback,
-               CSVLogger(os.path.join(run_dir, 'training_history.csv')),
+    callbacks=[CSVLogger(os.path.join(run_dir, 'training_history.csv')),
                LearningRateScheduler(schedule)],
     verbose=args.verbose,
     validation_freq=args.val_freq

13f6c3df5a961f053d990e575b7b331c7358338e
msangalli <mateussangalli@gmail.com>
2023-06-29 11:37:18 +0200

    add testing with batches

--- testing/test_size_regularization.py
+++ testing/test_size_regularization.py
@@ -26,6 +26,17 @@ class TestSizeRegularization(unittest.TestCase):
         b = QuadraticPenaltyHeight(1, 5.)(im)
         self.assertAlmostEqual(b, 25., "quadratic penalty should be close to 25 here")
 
+    def test_penalty_height_batch(self):
+        im = np.zeros([50, 40, 40, 3], np.float32)
+        im[:, 10:20, :, 1] = 1.
+        im[..., 0] = 1. - im[..., 1]
+        im = tf.constant(im)
+        a = QuadraticPenaltyHeight(1, 20.)(im)
+        self.assertAlmostEqual(a, 0., "quadratic penalty should be zero here")
+        b = QuadraticPenaltyHeight(1, 5.)(im)
+        print(b)
+        self.assertAlmostEqual(b, 25., "quadratic penalty should be close to 25 here")
+
 
 if __name__ == "__main__":
     unittest.main()

2563125448ab36688afc2510f5898b13624e73a0
msangalli <mateussangalli@gmail.com>
2023-07-06 17:03:18 +0200

    show more averages

--- average_run_per_params.py
+++ average_run_per_params.py
@@ -9,7 +9,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("runs_dir", type=str)
 args = parser.parse_args()
 
-ious = dict()
+results = dict()
 
 for run_id in os.listdir(args.runs_dir):
     run_path = os.path.join(args.runs_dir, run_id)
@@ -23,15 +23,20 @@ for run_id in os.listdir(args.runs_dir):
     with open(params_path, 'r') as fp:
         params = json.load(fp)
 
-    iou = df['0'][0]
+
     weight = params['max_weight']
     height_weight = params['height_reg_weight']
-
     key = f'({weight:.4f},{height_weight:.4f})'
-    if key not in ious:
-        ious[key] = []
-    ious[key].append(iou)
+    if key not in results:
+        results[key] = {}
+        for column in df:
+            results[key][column] = []
 
-for k, v in ious.items():
-    v = np.mean(v)
-    print(f'{k}: {v}')
+    for column in df:
+        results[key][column].append(df[column][0])
+
+for k, v in results.items():
+    print(f'{k}:')
+    for k2, v2 in v.items():
+        v2 = np.mean(v2)
+        print(f'   {k2}: {v2}')

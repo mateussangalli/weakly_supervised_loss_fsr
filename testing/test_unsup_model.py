491a315f86897d82bc4a5da1fbe10ccff0601b6e
msangalli <mateussangalli@gmail.com>
2023-07-12 19:46:03 +0200

    add tests for LogBarrier losses and UnsupUNet

--- testing/test_unsup_model.py
+++ testing/test_unsup_model.py
@@ -6,7 +6,9 @@ import tensorflow as tf
 from keras.layers import Dense, Input
 from keras.models import load_model
 
+from utils.size_regularization import LogBarrierHeight, LogBarrierHeightRatio
 from utils.unlabeled_training import UnsupModel
+from utils.unet import UnsupUNetBuilder
 
 
 class UpdatableLoss(tf.keras.regularizers.Regularizer):
@@ -24,10 +26,12 @@ class UpdatableLoss(tf.keras.regularizers.Regularizer):
 def get_and_compile_model():
     input_layer = Input((5,))
     out_layer = Dense(10)(input_layer)
-    model = UnsupModel(input_layer, out_layer, alpha=1.)
+    model = UnsupModel(input_layer, out_layer)
     def loss1(pred): return tf.reduce_max(tf.abs(pred))
     def loss2(pred): return tf.reduce_sum(tf.abs(pred))
-    model.compile('adam', {'loss1': loss1, 'loss2': loss2})
+    model.compile('adam',
+                  {'loss1': loss1, 'loss2': loss2},
+                  {'loss1': lambda _: 1., 'loss2': lambda _: 1.})
     return model
 
 
@@ -36,11 +40,12 @@ def get_dataset():
     ds = tf.data.Dataset.from_tensor_slices(data).batch(8)
     return ds
 
+
 class TestUnlabeledTraining(unittest.TestCase):
     def test_model_creation(self):
         input_layer = Input((5,))
         out_layer = Dense(10)(input_layer)
-        model = UnsupModel(input_layer, out_layer, alpha=1.)
+        model = UnsupModel(input_layer, out_layer)
         inputs = tf.random.uniform([20, 5])
         out = model(inputs)
         self.assertEqual(out.shape[0], 20)
@@ -101,8 +106,11 @@ class TestUnlabeledTraining(unittest.TestCase):
 
         input_layer = Input((5,))
         out_layer = Dense(10)(input_layer)
-        model = UnsupModel(input_layer, out_layer, alpha=[alpha_schedule1, alpha_schedule2])
-        model.compile('adam', {'loss1': loss1, 'loss2': loss2})
+        model = UnsupModel(input_layer, out_layer)
+        model.compile('adam',
+                      {'loss1': loss1, 'loss2': loss2},
+                      {'loss1': alpha_schedule1, 'loss2': alpha_schedule2}
+                      )
 
         ds = get_dataset()
 
@@ -125,8 +133,11 @@ class TestUnlabeledTraining(unittest.TestCase):
 
         input_layer = Input((5,))
         out_layer = Dense(10)(input_layer)
-        model = UnsupModel(input_layer, out_layer, alpha=[alpha_schedule1, alpha_schedule2])
-        model.compile('adam', {'loss1': loss1, 'loss2': loss2})
+        model = UnsupModel(input_layer, out_layer)
+        model.compile('adam',
+                      {'loss1': loss1, 'loss2': loss2},
+                      {'loss1': alpha_schedule1, 'loss2': alpha_schedule2}
+                      )
 
         ds = get_dataset()
 
@@ -136,78 +147,92 @@ class TestUnlabeledTraining(unittest.TestCase):
         dist = np.sum(np.abs(expected - results))
         self.assertAlmostEqual(dist, 0.)
 
-    # def test_unet(self):
-    #     model = SemiSupUNetBuilder((32, 32, 5), 2, 3, output_channels=3).build()
-    #     def loss2(pred): return tf.reduce_sum(tf.abs(pred))
-    #     model.compile('adam', 'mse', loss2, metrics=['mse'])
-    #
-    #     inputs_l = tf.random.uniform([20, 32, 32, 5])
-    #     outputs = tf.random.uniform([20, 32, 32, 3])
-    #     inputs_u = tf.random.uniform([40, 32, 32, 5])
-    #     inputs_l_val = tf.random.uniform([20, 32, 32, 5])
-    #     outputs_val = tf.random.uniform([20, 32, 32, 3])
-    #
-    #     ds_l = tf.data.Dataset.from_tensor_slices((inputs_l, outputs)).batch(4)
-    #     ds_u = tf.data.Dataset.from_tensor_slices(inputs_u).batch(8)
-    #     ds = tf.data.Dataset.zip((ds_l, ds_u))
-    #     ds_val = tf.data.Dataset.from_tensor_slices((inputs_l_val, outputs_val)).batch(4)
-    #
-    #     model.fit(ds, epochs=2, validation_data=ds_val, verbose=0)
-    #     model.evaluate(ds_val)
-    #
-    # def test_directional_loss(self):
-    #     model = SemiSupUNetBuilder((32, 32, 5), 2, 3, output_channels=3).build()
-    #     loss2 = PRPDirectionalPenalty(3, 5)
-    #     model.compile('adam', 'mse', loss2, metrics=['mse'])
-    #
-    #     inputs_l = tf.random.uniform([20, 32, 32, 5])
-    #     outputs = tf.random.uniform([20, 32, 32, 3])
-    #     inputs_u = tf.random.uniform([40, 32, 32, 5])
-    #     inputs_l_val = tf.random.uniform([20, 32, 32, 5])
-    #     outputs_val = tf.random.uniform([20, 32, 32, 3])
-    #
-    #     ds_l = tf.data.Dataset.from_tensor_slices((inputs_l, outputs)).batch(4)
-    #     ds_u = tf.data.Dataset.from_tensor_slices(inputs_u).batch(8)
-    #     ds = tf.data.Dataset.zip((ds_l, ds_u))
-    #     ds_val = tf.data.Dataset.from_tensor_slices((inputs_l_val, outputs_val)).batch(4)
-    #
-    #     model.fit(ds, epochs=2, validation_data=ds_val)
-    #     model.evaluate(ds_val)
-    #
-    # def test_multiple_losses(self):
-    #     def alpha_schedule1(t):
-    #         return tf.cast(t // 5, tf.float32)
-    #
-    #     def alpha_schedule2(t):
-    #         return .5 * tf.cast(t // 5, tf.float32)
-    #
-    #     def loss_l(y, y_pred): return tf.reduce_sum(tf.abs(y_pred + y)) * 1e-10
-    #     def loss_u1(y_pred): return tf.reduce_sum(tf.abs(y_pred)) * 1e-10 + 1.
-    #     def loss_u2(y_pred): return tf.reduce_sum(tf.abs(y_pred)) * 1e-10 + 1.
-    #
-    #     input_layer = Input((5,))
-    #     out_layer = Dense(10)(input_layer)
-    #     model = SemiSupModel(input_layer,
-    #                          out_layer,
-    #                          alpha=[alpha_schedule1, alpha_schedule2],
-    #                          num_unlabeled_losses=2)
-    #     model.compile('adam', loss_l, [loss_u1, loss_u2])
-    #
-    #     inputs_l = tf.random.uniform([20, 5])
-    #     outputs = tf.random.uniform([20, 10])
-    #     inputs_u = tf.random.uniform([40, 5])
-    #
-    #     ds_l = tf.data.Dataset.from_tensor_slices((inputs_l, outputs)).batch(4)
-    #     ds_u = tf.data.Dataset.from_tensor_slices(inputs_u).batch(8)
-    #     ds = tf.data.Dataset.zip((ds_l, ds_u))
-    #
-    #     history = model.fit(ds, epochs=15, verbose=0)
-    #     expected = 1.5 * np.arange(15).astype(np.float32)
-    #     results = np.array(history.history['total_loss'])
-    #     dist = np.sum(np.abs(expected - results))
-    #     self.assertAlmostEqual(dist, 0.)
+    def test_unet(self):
+        def alpha_schedule1(t):
+            return tf.random.uniform([])
+
+        def alpha_schedule2(t):
+            return tf.cast(t // 5, tf.float32)
+
+        model = UnsupUNetBuilder((32, 32, 5), 2, 3,
+                                 output_channels=3).build()
+        model.compile('adam',
+                      {'loss1': UpdatableLoss(), 'loss2': UpdatableLoss()},
+                      {'loss1': alpha_schedule1, 'loss2': alpha_schedule2},
+                      metrics=['mse'])
+
+        inputs = tf.random.uniform([40, 32, 32, 5])
+        inputs_l_val = tf.random.uniform([20, 32, 32, 5])
+        outputs_val = tf.random.uniform([20, 32, 32, 3])
+
+        ds = tf.data.Dataset.from_tensor_slices(inputs).batch(8)
+        ds_val = tf.data.Dataset.from_tensor_slices((inputs_l_val, outputs_val)).batch(4)
+
+        model.fit(ds, epochs=2, validation_data=ds_val, verbose=0)
+        model.evaluate(ds_val)
+
+    def test_log_barrier_height(self):
+        def alpha_schedule(step):
+            return 1.
+
+        def t_schedule(step):
+            return 1. + tf.cast(step, tf.float32) / 10
+        model = UnsupUNetBuilder((32, 32, 5), 2, 3, output_channels=3).build()
+        model.compile(tf.keras.optimizers.SGD(learning_rate=1e-2),
+                      {'loss1': LogBarrierHeight(0, 0.1, 1., t_schedule),
+                       'loss2': LogBarrierHeight(1, 0.1, 3., t_schedule)},
+                      {'loss1': alpha_schedule,
+                       'loss2': alpha_schedule},
+                      metrics=['mse'])
+
+        inputs = tf.zeros([1, 32, 32, 5])
+        inputs_l_val = tf.random.uniform([20, 32, 32, 5])
+        outputs_val = tf.random.uniform([20, 32, 32, 3])
+
+        ds = tf.data.Dataset.from_tensor_slices(inputs).batch(1)
+        ds_val = tf.data.Dataset.from_tensor_slices((inputs_l_val, outputs_val)).batch(4)
+
+        model.fit(ds, epochs=200, validation_data=ds_val, verbose=0)
+        model.evaluate(ds_val)
+        out = model(inputs[:1, ...])
+        out = np.array(out)[0, ...]
+        out_height = np.sum(out, 0)
+        out_mean_height = np.mean(out_height, 0)
+        print(f'{out_mean_height=}')
+        self.assertGreaterEqual(out_mean_height[0], 0.1)
+        self.assertLessEqual(out_mean_height[0], 1.1)
+        self.assertLessEqual(out_mean_height[1], 3.1)
+
+    def test_log_barrier_ratio(self):
+        def alpha_schedule(step):
+            return 1.
+
+        def t_schedule(step):
+            return 1. + tf.cast(step, tf.float32) / 10.
+        model = UnsupUNetBuilder((32, 32, 5), 2, 3, output_channels=3).build()
+        model.compile(tf.keras.optimizers.SGD(learning_rate=1e-2),
+                      {'loss1': LogBarrierHeightRatio(1, 2, 3., 5., t_schedule)},
+                      {'loss1': alpha_schedule},
+                      metrics=['mse'])
+
+        inputs = tf.zeros([1, 32, 32, 5])
+        inputs_l_val = tf.random.uniform([20, 32, 32, 5])
+        outputs_val = tf.random.uniform([20, 32, 32, 3])
+
+        ds = tf.data.Dataset.from_tensor_slices(inputs).batch(1)
+        ds_val = tf.data.Dataset.from_tensor_slices((inputs_l_val, outputs_val)).batch(4)
+
+        model.fit(ds, epochs=200, validation_data=ds_val, verbose=0)
+        model.evaluate(ds_val)
+        out = model(inputs[:1, ...])
+        out = np.array(out)[0, ...]
+        out_height = np.sum(out, 0)
+        out_ratio = out_height[:, 1] / out_height[:, 2]
+        out_mean_ratio = np.mean(out_ratio)
+        print(f'{out_mean_ratio=}')
+        self.assertGreaterEqual(out_mean_ratio, 2.9)
+        self.assertLessEqual(out_mean_ratio, 5.1)
 
 
 if __name__ == "__main__":
-    print(hasattr(UpdatableLoss(), 'update'))
     unittest.main()

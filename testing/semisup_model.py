b3b4dcbee72bb3446a9516b5ec2fab0bc1642b16
msangalli <mateussangalli@gmail.com>
2023-07-14 20:30:28 +0200

    use log barrier loss function for testing

--- testing/semisup_model.py
+++ testing/semisup_model.py
@@ -183,35 +183,35 @@ class TestUnlabeledTraining(unittest.TestCase):
         model.evaluate(ds_val)
 
     def test_direction_log_barrier(self):
-        def alpha_schedule(t):
-            return 10.
+        def alpha_schedule(step):
+            return tf.minimum(0.1 * tf.cast(step, tf.float32), 1.)
 
-        def t_schedule(t):
-            return 1. + tf.cast(t, tf.float32) * .1
+        def t_schedule(step):
+            return 1. + tf.cast(step, tf.float32) * .1
 
         penalty = PRPDirectionalPenalty(5, 1)
         loss_unlab = PRPDirectionalLogBarrier(
             5, 1, t_schedule, t=1., barrier_threshold=.3
         )
 
-        inputs_l = np.zeros([1, 32, 32, 5])
-        outputs_l = np.zeros([1, 32, 32, 3], np.float32)
+        inputs_l = np.zeros([10, 32, 32, 5])
+        outputs_l = np.zeros([10, 32, 32, 3], np.float32)
         outputs_l[:, 10:15, :, 0] = 1.
         outputs_l[:, 5:10, :, 2] = 1.
         outputs_l[..., 1] = 1. - outputs_l[..., 0] - outputs_l[..., 2]
         inputs_l[..., :3] = outputs_l - .5
         outputs_l = tf.constant(outputs_l)
         inputs_l = tf.constant(inputs_l)
-        inputs_u = tf.zeros([1, 32, 32, 5])
-        ds_l = tf.data.Dataset.from_tensor_slices((inputs_l, outputs_l)).batch(4)
-        ds_u = tf.data.Dataset.from_tensor_slices(inputs_u).batch(8)
+        inputs_u = inputs_l
+        ds_l = tf.data.Dataset.from_tensor_slices((inputs_l, outputs_l)).batch(2)
+        ds_u = tf.data.Dataset.from_tensor_slices(inputs_u).batch(2)
         ds = tf.data.Dataset.zip((ds_l, ds_u))
 
 
         model = SemiSupUNetBuilder((32, 32, 5), 2, 3, output_channels=3).build()
-        model.compile(tf.keras.optimizers.SGD(1e-2),
+        model.compile(tf.keras.optimizers.SGD(1e-2, momentum=.9),
                       tf.keras.losses.CategoricalCrossentropy(),
-                      {'loss_unlab': UpdatableLoss()},
+                      {'loss_unlab': loss_unlab},
                       {'loss_unlab': alpha_schedule},
                       metrics=['mse'])
         # TEST: uncomment this to test without regularization (should fail)

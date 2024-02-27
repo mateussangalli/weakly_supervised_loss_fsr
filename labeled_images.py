28396b7d8835f8a87a4c7852c555321e5dadca0f
msangalli <mateussangalli@gmail.com>
2024-02-27 15:47:19 +0100

    add correct names

--- labeled_images.py
+++ labeled_images.py
@@ -1,11 +1,15 @@
+# First three were chose for variability. The rest was randomly chosen.
 LABELED_IMAGES = [
-    'image_name',
-    'image_name',
-    'image_name'
-    ]
-
-LABELED_IMAGES_VAL = [
-    'image_name',
-    'image_name',
-    'image_name'
+    "train127",
+    "train034",
+    "train040",
+    "train028",
+    "train006",
+    "train072",
+    "train064",
+    "train059",
+    "train036",
+    "train026",
 ]
+
+LABELED_IMAGES_VAL = ["val010", "val20", "val30"]
